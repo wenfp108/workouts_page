f62dd579b06e152e8a5ec1030386ed9371293964
Woon Wen <43333282+wenfp108@users.noreply.github.com>
2024-12-01 11:21:22 +0800

    Update site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -12,7 +12,7 @@ interface ISiteMetadataResult {
 
 const data: ISiteMetadataResult = {
   siteTitle: 'Workouts Map',
-  siteUrl: 'https://app.follow.is/share/users/wenfp108',
+  siteUrl: 'https://app.follow.is/share/lists/71885160939929600',
   logo:  'https://i.ibb.co/fxcj8V7/Big-Meow-cats-catsofinstagram-cat-catstagram-instagram-catlover-catlife-catlovers-instacat-kitten-pe.jpg',
   description: 'Personal site and blog',
   navLinks: [
