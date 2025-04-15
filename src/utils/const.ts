d8b6b0eaa2de3a1ac6e19dceeb0f0f64be09d9fd
Leo_Woon <wenfp108@gmail.com>
2025-04-15 21:16:52 +0800

    update

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,6 +1,6 @@
 // const
 const MAPBOX_TOKEN =
-    // For security reasons, please avoid using the default public token provided by Mapbox as much as possible.
+  // For security reasons, please avoid using the default public token provided by Mapbox as much as possible.
   // Instead, manually add a new token and apply URL restrictions.
   // (please refer to https://github.com/yihong0618/running_page/issues/643#issuecomment-2042668580)
   'pk.eyJ1IjoiYmVuLTI5IiwiYSI6ImNrZ3Q4Ym9mMDBqMGYyeXFvODV2dWl6YzQifQ.gSKoWF-fMjhzU67TuDezJQ';
@@ -44,6 +44,8 @@ const PRIVACY_MODE = false;
 // update for now 2024/11/17 the lights on default is false
 //set to `false` if you want to make light off as default, only effect when `PRIVACY_MODE` = false
 const LIGHTS_ON =false;
+// richer title for the activity types (like garmin style)
+const RICH_TITLE = true;
 
 // IF you outside China please make sure IS_CHINESE = false
 const IS_CHINESE = true;
@@ -65,7 +67,6 @@ const HALF_MARATHON_RUN_TITLE = IS_CHINESE ? '半程马拉松' : 'Half Marathon'
 const RUN_TITLE = IS_CHINESE ? '跑步' : 'Run';
 const TRAIL_RUN_TITLE = IS_CHINESE ? '越野跑' : 'Trail Run';
 const SWIM_TITLE = IS_CHINESE ? '游泳' : 'Swim';
-
 const RIDE_TITLE = IS_CHINESE ? '骑行' : 'Ride';
 const INDOOR_RIDE_TITLE = IS_CHINESE ? '室内骑行' : 'Indoor Ride';
 const VIRTUAL_RIDE_TITLE = IS_CHINESE ? '虚拟骑行' : 'Virtual Ride';
@@ -76,6 +77,19 @@ const SNOWBOARD_TITLE = IS_CHINESE ? '单板滑雪' : 'Snowboard';
 const SKI_TITLE = IS_CHINESE ? '双板滑雪' : 'Ski';
 const ROAD_TRIP_TITLE = IS_CHINESE ? '自驾' : 'RoadTrip';
 const FLIGHT_TITLE = IS_CHINESE ? '飞行' : 'Flight';
+const RUN_TREADMILL_TITLE = IS_CHINESE ? '跑步机' : 'Treadmill Run';
+
+const ACTIVITY_COUNT_TITLE = IS_CHINESE ? '活动次数' : 'Activity Count';
+const MAX_DISTANCE_TITLE = IS_CHINESE ? '最远距离' : 'Max Distance';
+const MAX_SPEED_TITLE = IS_CHINESE ? '最快速度' : 'Max Speed';
+const TOTAL_TIME_TITLE = IS_CHINESE ? '总时间' : 'Total Time';
+const AVERAGE_SPEED_TITLE = IS_CHINESE ? '平均速度' : 'Average Speed';
+const TOTAL_DISTANCE_TITLE = IS_CHINESE ? '总距离' : 'Total Distance';
+const YEARLY_TITLE = IS_CHINESE ? '按年' : 'Yearly';
+const MONTHLY_TITLE = IS_CHINESE ? '按月' : 'Monthly';
+const WEEKLY_TITLE = IS_CHINESE ? '按周' : 'Weekly';
+const DAILY_TITLE = IS_CHINESE ? '按天' : 'Daily';
+const LOCATION_TITLE = IS_CHINESE ? '位置' : 'Location';
 
 const RUN_TITLES = {
   FULL_MARATHON_RUN_TITLE,
@@ -83,17 +97,46 @@ const RUN_TITLES = {
   RUN_TITLE,
   TRAIL_RUN_TITLE,
 
+  SWIM_TITLE,
   RIDE_TITLE,
   INDOOR_RIDE_TITLE,
   VIRTUAL_RIDE_TITLE,
   HIKE_TITLE,
   ROWING_TITLE,
   KAYAKING_TITLE,
-  SWIM_TITLE,
-  ROAD_TRIP_TITLE,
-  FLIGHT_TITLE,
   SNOWBOARD_TITLE,
   SKI_TITLE,
+  ROAD_TRIP_TITLE,
+  FLIGHT_TITLE,
+  RUN_TREADMILL_TITLE,
+};
+
+const TYPES_MAPPING = {
+  'run': RUN_TITLES.RUN_TITLE,
+  'trail run': RUN_TITLES.TRAIL_RUN_TITLE,
+  'swim': RUN_TITLES.SWIM_TITLE,
+  'ride': RUN_TITLES.RIDE_TITLE,
+  'virtualride': RUN_TITLES.VIRTUAL_RIDE_TITLE,
+  'hike': RUN_TITLES.HIKE_TITLE,
+  'rowing': RUN_TITLES.ROWING_TITLE,
+  'kayaking': RUN_TITLES.KAYAKING_TITLE,
+  'snowboard': RUN_TITLES.SNOWBOARD_TITLE,
+  'ski': RUN_TITLES.SKI_TITLE,
+  'roadtrip': RUN_TITLES.ROAD_TRIP_TITLE,
+}
+
+const ACTIVITY_TOTAL = {
+    ACTIVITY_COUNT_TITLE,
+    MAX_DISTANCE_TITLE,
+    MAX_SPEED_TITLE,
+    TOTAL_TIME_TITLE,
+    AVERAGE_SPEED_TITLE,
+    TOTAL_DISTANCE_TITLE,
+    YEARLY_TITLE,
+    MONTHLY_TITLE,
+    WEEKLY_TITLE,
+    DAILY_TITLE,
+    LOCATION_TITLE
 };
 
 export {
@@ -114,6 +157,9 @@ export {
   MAP_HEIGHT,
   PRIVACY_MODE,
   LIGHTS_ON,
+  RICH_TITLE,
+  ACTIVITY_TOTAL,
+  TYPES_MAPPING,
 };
 
 const nike = 'rgb(224,237,94)';
@@ -127,14 +173,13 @@ const gold = 'rgb(242,190,69)';
 const purple = 'rgb(154,118,252)';
 const veryPeri = 'rgb(105,106,173)';//长春花蓝
 const red = 'rgb(255,0,0)';//大红色
-const blue = 'rgb(94, 153, 237)';
 
 // If your map has an offset please change this line
 // issues #92 and #198
 export const NEED_FIX_MAP = false;
 export const MAIN_COLOR = green;
 export const RUN_COLOR = yellow;
-export const RIDE_COLOR = blue;
+export const RIDE_COLOR = green;
 export const VIRTUAL_RIDE_COLOR = veryPeri;
 export const HIKE_COLOR = pink;
 export const SWIM_COLOR = gold;
