2a5de8689394a05ff414e815ab3b34330212ba4d
Leo Woon <43333282+wenfp108@users.noreply.github.com>
2025-04-15 23:10:54 +0800

    Update const.ts

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -173,16 +173,16 @@ const gold = 'rgb(242,190,69)';
 const purple = 'rgb(154,118,252)';
 const veryPeri = 'rgb(105,106,173)';//长春花蓝
 const red = 'rgb(255,0,0)';//大红色
-const MediumBlue = 'rgb(0,0,205)';
-const DarkGreen = 'rgb(0,100,0)';
+const DeepSkyBlue = 'rgb(0,191,255)';
+const SpringGreen = 'rgb(0,255,127)';
 // If your map has an offset please change this line
 // issues #92 and #198
 export const NEED_FIX_MAP = false;
 export const MAIN_COLOR = green;
 export const RUN_COLOR = yellow;
-export const RIDE_COLOR = DarkGreen;
+export const RIDE_COLOR = DeepSkyBlue;
 export const VIRTUAL_RIDE_COLOR = veryPeri;
-export const HIKE_COLOR = MediumBlue;
+export const HIKE_COLOR = SpringGreen;
 export const SWIM_COLOR = gold;
 export const ROWING_COLOR = cyan;
 export const ROAD_TRIP_COLOR = purple;
